ce67b09362ead5e88cac92e91aba2204a44ade9a
Walid Budgaga <wbudgaga@gmail.com>
2020-07-24 12:17:18 -0600

    Update AcceptorListener.java

--- mr/communication/handlers/AcceptorListener.java
+++ mr/communication/handlers/AcceptorListener.java
@@ -6,14 +6,14 @@ import java.nio.channels.SocketChannel;
 public interface AcceptorListener {
   /**
    * Called when a connection is established.
-   * @param acceptor The acceptor that originated this event. 
-   * @param sc The newly connected socket.
+   * @param acceptor: The acceptor that originated this event. 
+   * @param sc: The newly connected socket.
    */
   public void socketConnected(Acceptor acceptor, SocketChannel sc);
   /**
    * Called when an error occurs on the Acceptor.
-   * @param acceptor The acceptor where the error occured.
-   * @param ex The exception representing the error.
+   * @param acceptor: The acceptor where the error occured.
+   * @param ex: The exception representing the error.
    */
   public void socketError(Acceptor acceptor, Exception ex);
 }
