0dcfa0342cc579fa645098ad822ee99e4f2a9c3e
Walid Budgaga <wbudgaga@gmail.com>
2020-07-26 12:25:45 -0600

    Update SimpleProtocolDecoder.java

--- mr/communication/handlers/SimpleProtocolDecoder.java
+++ mr/communication/handlers/SimpleProtocolDecoder.java
@@ -13,8 +13,8 @@ import mr.dht.peer2peernetwork.wireformates.Predecessor;
 
 final public class SimpleProtocolDecoder implements ProtocolDecoder {
 	private final static int 	BUFFER_SIZE 	= 10*1024;
-	private byte[] 			buffer 			= new byte[BUFFER_SIZE];
-	private int 			pos 			= 0;
+	private byte[] 			buffer 		= new byte[BUFFER_SIZE];
+	private int 			pos 		= 0;
 	private int 			stillNeededBytes= 0;
 	private final MessageFactory messageFactory;
   
