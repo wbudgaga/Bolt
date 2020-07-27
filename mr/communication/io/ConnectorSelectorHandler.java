5d4ef9ada3af1941dd3c2699a4007604f44531f5
Walid Budgaga <wbudgaga@gmail.com>
2020-07-27 10:06:55 -0600

    Update ConnectorSelectorHandler.java

--- mr/communication/io/ConnectorSelectorHandler.java
+++ mr/communication/io/ConnectorSelectorHandler.java
@@ -7,8 +7,6 @@ package mr.communication.io;
  * 
  * Should be implemented by classes wishing to be notified 
  * when a Socket finishes connecting to a remote point. 
- * 
- * @author Nuno Santos
  */
 public interface ConnectorSelectorHandler extends SelectorHandler {
   /**
