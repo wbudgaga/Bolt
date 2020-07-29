621121a8cb823180a27c0ec7e66cdd610582d467
Walid Budgaga <wbudgaga@gmail.com>
2020-07-29 11:50:59 -0600

    Update SelectorThread.java

--- mr/communication/io/SelectorThread.java
+++ mr/communication/io/SelectorThread.java
@@ -230,7 +230,7 @@ final public class SelectorThread implements Runnable {
     
   private void closeSelectorAndChannels() {
     Set keys 				= selector.keys();
-    for (Iterator iter 		= keys.iterator(); iter.hasNext();) {
+    for (Iterator iter 			= keys.iterator(); iter.hasNext();) {
       SelectionKey key 		= (SelectionKey)iter.next();
       try {
         key.channel().close();
