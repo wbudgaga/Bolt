13b2c6a235272ccc25bcdb62336b2436af057470
Walid Budgaga <wbudgaga@gmail.com>
2020-07-30 11:47:35 -0600

    Update TestReadFromFile.java

--- mr/dht/peer2peernetwork/benchmark/TestReadFromFile.java
+++ mr/dht/peer2peernetwork/benchmark/TestReadFromFile.java
@@ -97,8 +97,8 @@ public class TestReadFromFile {
 		MappedByteBuffer mb 		= fChannel.map( FileChannel.MapMode.READ_ONLY, Integer.MAX_VALUE * (long)i ,fChannel.size( ) - Integer.MAX_VALUE * (long)i);
 		
 		while (mb.hasRemaining()){
-			int len = mb.remaining()>bytes.length?bytes.length:mb.remaining();
-			mb.get(bytes,0,len);			
+			int len 		= mb.remaining()>bytes.length?bytes.length:mb.remaining();
+			mb.get(bytes, 0, len);			
 		}
 
 		long end = System.currentTimeMillis();
