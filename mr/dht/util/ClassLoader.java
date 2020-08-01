786968448c4818a356fd54d27f1fbad341f57b14
Walid Budgaga <wbudgaga@gmail.com>
2020-08-01 13:47:09 -0600

    Update ClassLoader.java

--- mr/dht/util/ClassLoader.java
+++ mr/dht/util/ClassLoader.java
@@ -40,13 +40,12 @@ public class ClassLoader {
 
 		URL[] urls 			= { new URL("jar:file:" + path+"!/") };
 		URLClassLoader cl 		= URLClassLoader.newInstance(urls);
-
-		    while (e.hasMoreElements()) {
-		        JarEntry je 		= (JarEntry) e.nextElement();
+		while (e.hasMoreElements()) {
+			JarEntry je 		= (JarEntry) e.nextElement();
 		        if(je.isDirectory() || !je.getName().endsWith(".class")){
 		            continue;
 		        }
-		    // -6 because of .class
+		// -6 because of .class
 		    String className 		= je.getName().substring(0,je.getName().length()-6);
 		    className 			= className.replace('/', '.');
 		    Class c 			= cl.loadClass(className);
