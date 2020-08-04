87959681bd39bb2c5738636e5408e4301fb2706f
Walid Budgaga <wbudgaga@gmail.com>
2020-08-04 11:12:39 -0600

    Update MapTaskDataProvider.java

--- mr/resourcemanagement/io/MapTaskDataProvider.java
+++ mr/resourcemanagement/io/MapTaskDataProvider.java
@@ -9,24 +9,28 @@ import mr.resourcemanagement.datatype.TaskData;
 import mr.resourcemanagement.io.DataSource;
 
 public class MapTaskDataProvider extends Task{
-	private boolean 	stillRunning = true;
+	private boolean 	stillRunning 	= true;
 	private DataSource	fileReader;
 	
 	public void setDataSource(DataSource fileReader){
-		this.fileReader = fileReader;
+		this.fileReader 		= fileReader;
 	}
+	
 	@Override
 	public void execute() throws IOException, InterruptedException {
 		fileReader.passNextDataList();
 		stopRunning();
 	}
+	
 	public void returnTaskDataObject(TaskData td) {
 		fileReader.returnTaskDataObject(td);
 	}
+	
 	public boolean isStillRunning() {
 		return stillRunning;
 	}
+	
 	public void stopRunning() {
-		this.stillRunning = false;
+		this.stillRunning 		= false;
 	}
 }
