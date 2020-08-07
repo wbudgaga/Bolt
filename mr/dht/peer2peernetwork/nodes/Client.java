2e406cd4790ab98a647f17b1b5a73544e99bec19
Walid Budgaga <wbudgaga@gmail.com>
2020-08-07 13:04:04 -0600

    Update Client.java

--- mr/dht/peer2peernetwork/nodes/Client.java
+++ mr/dht/peer2peernetwork/nodes/Client.java
@@ -34,9 +34,9 @@ import mr.resourcemanagement.execution.mrtasks.management.JobTasksManager;
 
 public class Client  extends LNode implements CMDLineInterface{
 	public RemotePeer remotePeer;
-	private final Object LOCK 					= new Object();
-	private ConcurrentHashMap <String,Long> pendingJobs = new ConcurrentHashMap<String,Long>();//<jobID+"_"+TaskID,rKey>
-	private ConcurrentHashMap <Long,BlockingQueue<StartMapTask>> pendingJobTasks = new ConcurrentHashMap<Long,BlockingQueue<StartMapTask>>();//<jobID,<tasks>>
+	private final Object LOCK 							= new Object();
+	private ConcurrentHashMap <String,Long> pendingJobs 				= new ConcurrentHashMap<String,Long>();//<jobID+"_"+TaskID,rKey>
+	private ConcurrentHashMap <Long,BlockingQueue<StartMapTask>> pendingJobTasks 	= new ConcurrentHashMap<Long,BlockingQueue<StartMapTask>>();//<jobID,<tasks>>
 	protected ConcurrentHashMap <Long,RemotePeer> cachedPeers = new ConcurrentHashMap<Long,RemotePeer>();
 	private ConcurrentHashMap <Long,TextNumTaskData> pendingTasksData = new ConcurrentHashMap<Long,TextNumTaskData>();
 	public boolean 	allJobSubmitted = false;
