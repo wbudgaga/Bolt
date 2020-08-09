6c4f0f427e53661743af8885883b99a0a9a65c89
Walid Budgaga <wbudgaga@gmail.com>
2020-08-09 12:59:44 -0600

    Update CircularBuffer.java

--- mr/dht/peer2peernetwork/datastructure/CircularBuffer.java
+++ mr/dht/peer2peernetwork/datastructure/CircularBuffer.java
@@ -4,23 +4,20 @@ import java.nio.BufferOverflowException;
 import java.nio.BufferUnderflowException;
 
 public class CircularBuffer<T> {
-
 	  private T[] buffer;
-
 	  private int tail;
-
 	  private int head;
 
 	  @SuppressWarnings("unchecked")
 	  public CircularBuffer(int n) {
-	    buffer = (T[]) new Object[n];
-	    tail = 0;
-	    head = 0;
+	    buffer		 	= (T[]) new Object[n];
+	    tail 			= 0;
+	    head 			= 0;
 	  }
 
 	  public void add(T toAdd) {
 	    if (head != (tail - 1)) {
-	        buffer[head++] = toAdd;
+	        buffer[head++] 		= toAdd;
 	    } else {
 	        throw new BufferOverflowException();
 	    }
@@ -29,10 +26,10 @@ public class CircularBuffer<T> {
 
 	  public T get() {
 	    T t = null;
-	    int adjTail = tail > head ? tail - buffer.length : tail;
+	    int adjTail 		= tail > head ? tail - buffer.length : tail;
 	    if (adjTail < head) {
-	        t = (T) buffer[tail++];
-	        tail = tail % buffer.length;
+	        t 			= (T) buffer[tail++];
+	        tail 			= tail % buffer.length;
 	    } else {
 	        throw new BufferUnderflowException();
 	    }
