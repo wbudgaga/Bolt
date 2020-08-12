35e00670be2ce6928f86880b29dad480c2eca730
Walid Budgaga <wbudgaga@gmail.com>
2020-08-12 09:58:59 -0600

    Update ForwardHandler.java

--- mr/dht/peer2peernetwork/handlers/ForwardHandler.java
+++ mr/dht/peer2peernetwork/handlers/ForwardHandler.java
@@ -10,7 +10,7 @@ public class ForwardHandler extends LookupHandler{
 	public void handle(PacketChannel pc, Message msg) {
 		Lookup forward = (Lookup) msg;
 		if (forward.getSourcePeer().getPeer().getPeerID() == getNode().getID() || forward.getHops() > Setting.NUMBER_OF_FT_ENTRIES ){
-			System.out.println(forward.getSourcePeer().getPeer().getPeerID() +"There is something wrong in handling forward MSG"+forward.getHops()+"   "+getNode().getID());
+			System.out.println(forward.getSourcePeer().getPeer().getPeerID() + "There is something wrong in handling forward MSG" + forward.getHops()+"   "+getNode().getID());
 			return;
 		}
 		lookup(forward);
