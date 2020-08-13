651b4de04f1a5a3acb1d0b1d538238689e92877d
Walid Budgaga <wbudgaga@gmail.com>
2020-08-13 11:34:57 -0600

    Update MessageHandler.java

--- mr/dht/peer2peernetwork/handlers/MessageHandler.java
+++ mr/dht/peer2peernetwork/handlers/MessageHandler.java
@@ -9,21 +9,21 @@ import mr.dht.peer2peernetwork.util.ByteStream;
 import mr.dht.peer2peernetwork.wireformates.FinishedMapTaskNotify;
 import mr.dht.peer2peernetwork.wireformates.Message;
 
-
 public abstract  class MessageHandler {
 	protected LNode node;
 	
 	public void setNode(LNode node){
-		this.node = node;
+		this.node 				= node;
 	}
+	
 	public abstract int getHandlerID();
 	public abstract void handle(PacketChannel pc, Message msg);	
 	
 	public static void sendMessage(PacketChannel pc, Message msg) {
 		sendMessage(pc, ByteStream.addPacketHeader(msg.packMessage()));
 	}
+	
 	private static void sendMessage(PacketChannel pc, byte[] bytesToSend) {
 		pc.sendPacket(ByteBuffer.wrap(bytesToSend));
 	}
-
 }
