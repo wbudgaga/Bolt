59c48b6124f9f0bd99b5ff0ef8d3bb8b89d9f724
Walid Budgaga <wbudgaga@gmail.com>
2020-08-14 10:56:39 -0600

    Update StoreDataMessageHandler.java

--- mr/dht/peer2peernetwork/handlers/StoreDataMessageHandler.java
+++ mr/dht/peer2peernetwork/handlers/StoreDataMessageHandler.java
@@ -1,6 +1,5 @@
 package mr.dht.peer2peernetwork.handlers;
 
-
 import mr.communication.handlers.PacketChannel;
 import mr.dht.peer2peernetwork.nodes.Peer;
 import mr.dht.peer2peernetwork.wireformates.Message;
@@ -8,14 +7,16 @@ import mr.dht.peer2peernetwork.wireformates.StoreFileRequest;
 import mr.resourcemanagement.execution.mrtasks.management.CopyAndForwardTask;
 
 public class StoreDataMessageHandler extends MessageHandler{
+	
 	public void storeChunk(PacketChannel pc, StoreFileRequest chunkMSG) {
 		//System.out.println(node.getID()+" Chunk part recieved>> "+chunkMSG.getFileName());
-		Peer lp = (Peer) node; 
+		Peer lp					= (Peer) node; 
 		if (!lp.inPeerRange(Long.parseLong(chunkMSG.getFileName()))){
-			System.out.println("Peer (ID:"+lp.getID()+") received stronger chunk with id: "+chunkMSG.getFileName()+"   "+pc.getSocketChannel().socket());
+			System.out.println("Peer (ID:" + lp.getID() + ") received stronger chunk with id: " + chunkMSG.getFileName() + "   " + pc.getSocketChannel().socket());
 			//System.exit(0);
 		}
-		CopyAndForwardTask cfTask = new CopyAndForwardTask(chunkMSG, lp);
+		
+		CopyAndForwardTask cfTask 		= new CopyAndForwardTask(chunkMSG, lp);
 		lp.getResourceManager().executeTask(cfTask);
 	}	
 
