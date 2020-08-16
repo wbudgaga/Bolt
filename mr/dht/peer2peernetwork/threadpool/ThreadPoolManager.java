6aeb99612597c8fec377939e3f3203247100b04f
Walid Budgaga <wbudgaga@gmail.com>
2020-08-16 11:45:31 -0600

    Update ThreadPoolManager.java

--- mr/dht/peer2peernetwork/threadpool/ThreadPoolManager.java
+++ mr/dht/peer2peernetwork/threadpool/ThreadPoolManager.java
@@ -18,11 +18,12 @@ public class ThreadPoolManager{
 	}
 		
 	public void createWorkers(int numberOfThreads){
-		workers 		= new Worker[numberOfThreads];
+		workers 				= new Worker[numberOfThreads];
 		for (int i = 0; i < numberOfThreads; ++i) {
-			workers[i] 	= new Worker(this, i);
+			workers[i] 			= new Worker(this, i);
 		}
 	}
+	
 	public synchronized boolean isIdle(){
 		if (taskQueue.size() == 0 && workerQueue.size() == getSize())
 			return true;
