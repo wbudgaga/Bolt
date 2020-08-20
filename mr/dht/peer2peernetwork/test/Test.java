924460c6f60357a1aeaedd4a41765ecc14a370c4
Walid Budgaga <wbudgaga@gmail.com>
2020-08-20 12:17:42 -0600

    Update Test.java

--- mr/dht/peer2peernetwork/test/Test.java
+++ mr/dht/peer2peernetwork/test/Test.java
@@ -21,7 +21,7 @@ public class Test<T> {
 		idx 			= (idx +1) % 500;
 		System.out.println(idx);
 		int[] lst 		= {2,4,6,8,0};
-		int[] l1 = Arrays.copyOfRange(lst,1,3);
+		int[] l1 		= Arrays.copyOfRange(lst,1,3);
 		System.out.println(l1.length);
 		System.out.println("StartMqapTask".contains("Map")+"    mm="+Integer.MAX_VALUE/1000000+"    "+("ahbsvext.xt".endsWith(ext)));
 		System.out.println("abc. Thm>q \'".replaceAll("[^a-zA-Z]", " "));
