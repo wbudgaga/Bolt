37d649e9d0ab4e45c01026d9eedeeb32649227ee
Walid Budgaga <wbudgaga@gmail.com>
2020-08-21 14:04:07 -0600

    Update JobTasksSubmitter.java

--- mr/dht/peer2peernetwork/nodes/JobTasksSubmitter.java
+++ mr/dht/peer2peernetwork/nodes/JobTasksSubmitter.java
@@ -18,7 +18,7 @@ public class JobTasksSubmitter  extends Task{
 	private final JobSubmitter	jobSubmitter;
 	private JobDescriptor		job;
 	private BlockingQueue<Long>  chunksIDs	= new ArrayBlockingQueue<Long>(1000);
-	private volatile int 		numOfChunks=0;
+	private volatile int 	numOfChunks     = 0;
 	
 	public JobTasksSubmitter(JobSubmitter jobSubmitter) {
 		this.jobSubmitter = jobSubmitter;
