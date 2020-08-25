fd32103fad8124fd177a724b72e80e489843a1ff
Walid Budgaga <wbudgaga@gmail.com>
2020-08-25 12:03:47 -0600

    Update ByteStream.java

--- mr/dht/peer2peernetwork/util/ByteStream.java
+++ mr/dht/peer2peernetwork/util/ByteStream.java
@@ -83,11 +83,11 @@ public class ByteStream {
 			        return null;
 			}
 				
-				byte[] bytes = new byte[(int)length];
-			    // Read in the bytes
-			    int offset = 0;
-			    int numRead = 0;
-			    while (offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+			byte[] bytes = new byte[(int)length];
+			// Read in the bytes
+			int offset = 0;
+			int numRead = 0;
+			while (offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
 			        offset += numRead;
 			    }
 			    if (offset < bytes.length) {
