4bd545635e21bc887812c55510507e0d2feeac01
Walid Budgaga <wbudgaga@gmail.com>
2020-08-26 11:53:40 -0600

    Update ByteStream.java

--- mr/dht/peer2peernetwork/util/ByteStream.java
+++ mr/dht/peer2peernetwork/util/ByteStream.java
@@ -76,8 +76,8 @@ public class ByteStream {
 		
 	public static byte[] readFileBytes(File file){
 		try {
-			InputStream is = new FileInputStream(file);
-			long length = file.length();
+			InputStream is	= new FileInputStream(file);
+			long length 	= file.length();
 			if (length > Integer.MAX_VALUE) {
 				System.out.println("File is too large. It can not be transfered!");
 			        return null;
@@ -85,10 +85,10 @@ public class ByteStream {
 				
 			byte[] bytes = new byte[(int)length];
 			// Read in the bytes
-			int offset = 0;
-			int numRead = 0;
-			while (offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
-				offset += numRead;
+			int offset 	= 0;
+			int numRead 	= 0;
+			while (offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length - offset)) >= 0) {
+				offset 	+= numRead;
 			}
 			if (offset < bytes.length) {
 				throw new IOException("Could not completely read file "+file.getName());
