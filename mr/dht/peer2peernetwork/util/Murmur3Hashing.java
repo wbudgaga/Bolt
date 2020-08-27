c477b2729313ff40e371a877256f44fb543b60fa
Walid Budgaga <wbudgaga@gmail.com>
2020-08-27 14:18:15 -0600

    Update Murmur3Hashing.java

--- mr/dht/peer2peernetwork/util/Murmur3Hashing.java
+++ mr/dht/peer2peernetwork/util/Murmur3Hashing.java
@@ -130,8 +130,8 @@ public class Murmur3Hashing {
     }
 
     private void applyKs(long k1, long k2) {
-        k1 *= C1;
-        k1 = Long.rotateLeft(k1, 31);
+        k1 				*= C1;
+        k1 				= Long.rotateLeft(k1, 31);
         k1 *= C2;
         h1 ^= k1;
 
