e54baf1b540f48798bfae54b4cb1cc2c795b2653
Walid Budgaga <wbudgaga@gmail.com>
2020-08-28 12:00:47 -0600

    Update FindRunningReducer.java

--- mr/dht/peer2peernetwork/wireformates/FindRunningReducer.java
+++ mr/dht/peer2peernetwork/wireformates/FindRunningReducer.java
@@ -51,21 +51,20 @@ public class FindRunningReducer extends Lookup{
 	}
 	
 	public static void main(String[] a){
-		FindRunningReducer f = new FindRunningReducer();
+		FindRunningReducer f 	= new FindRunningReducer();
 		f.setJobID(11);
 		f.setQueryKey(1234618);
-		PeerInfo sourcePeer = new PeerInfo();
-		PeerData pd = new PeerData();
+		PeerInfo sourcePeer 	= new PeerInfo();
+		PeerData pd 		= new PeerData();
 		pd.setHost("red-rock");
 		pd.setNickName("walid");
 		pd.setPeerID(102);
 		pd.setPortNum(1201);
 		sourcePeer.setPeer(pd);
 		f.setSourcePeer(sourcePeer);
-		byte[] b = f.packMessage();
-		FindRunningReducer f1 = new FindRunningReducer();
+		byte[] b 		= f.packMessage();
+		FindRunningReducer f1 	= new FindRunningReducer();
 		f1.initiate(b);
 		System.out.println(f1.getHandlerID()+" "+f1.getHops()+" "+f1.getJobID()+" "+f1.getMessageID()+" "+f1.getMessageID()+" "+f1.getMessageType()+" "+f1.getMsgUUID()+" "+f1.getQueryKey()+" "+f1.getSourcePeer().getPeer().getHost());
-		
 	}
 }
