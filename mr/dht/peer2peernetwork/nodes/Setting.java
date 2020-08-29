566e6a0b6d304959618fdd381c72622d97e17ace
Walid Budgaga <wbudgaga@gmail.com>
2020-08-29 13:10:07 -0600

    Update Setting.java

--- mr/dht/peer2peernetwork/nodes/Setting.java
+++ mr/dht/peer2peernetwork/nodes/Setting.java
@@ -70,12 +70,12 @@ public abstract class Setting {
 	public static int THREADPOOL_SIZE 		= 10;
 	
 	public static int INPUT_QUEUESIZE 		= 50;
-	public static int NUM_MAPBUFFERS	 		= 50;
+	public static int NUM_MAPBUFFERS	 	= 50;
 	public static int NUM_REDUCEBUFFERS		= 50;
 	
 	public static int KILO					 	= 1024;
 	public static int MEGA					 	= (int) Math.pow(1024,2);
-	public static int 	CHUNK_SIZE		 			= 128 * MEGA;
+	public static int CHUNK_SIZE		 			= 128 * MEGA;
 	public static int 	RECEIVEBUFF_SIZE		 	= 1 * MEGA;
 	public static int 	SENDBUFF_SIZE		 		= 800 * KILO;
 //	public static int 	REDUCER_BUFSIZE1		 	= 200 * MEGA;
