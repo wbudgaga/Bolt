46defa3cd3d9bbdfd5251ae7c1d930b1b08add1d
Walid Budgaga <wbudgaga@gmail.com>
2020-08-30 13:26:43 -0600

    Update FingerTableRequest.java

--- mr/dht/peer2peernetwork/wireformates/FingerTableRequest.java
+++ mr/dht/peer2peernetwork/wireformates/FingerTableRequest.java
@@ -1,6 +1,5 @@
 package mr.dht.peer2peernetwork.wireformates;
 
-
 public class FingerTableRequest extends PeerInfo{
 	public FingerTableRequest() {
 		super(FINGER_TABLE_REQUEST, FINGER_TABLE_REQUEST);
