0223866d9557af8d6f8286060714dffdbf9b3cd9
Walid Budgaga <wbudgaga@gmail.com>
2020-09-01 11:24:37 -0600

    Update TestWriteToFile.java

--- mr/dht/peer2peernetwork/test/TestWriteToFile.java
+++ mr/dht/peer2peernetwork/test/TestWriteToFile.java
@@ -38,10 +38,10 @@ public class TestWriteToFile {
 	public void storeUsingFileWriterAsOnce(String fn) throws IOException{
 		FileWriter f 			= new FileWriter(fn);
 		String txt			= "";
-		   long start 			= System.currentTimeMillis();
-		   for (int i = 0; i < RECORD_COUNT; ++i) {
-		        txt			+= RECORD;
-		    }
+		long start 			= System.currentTimeMillis();
+		for (int i = 0; i < RECORD_COUNT; ++i) {
+			txt			+= RECORD;
+		}
 		   f.write(txt);
 		   f.flush();
 		   f.close();
