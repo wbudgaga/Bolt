98f01d7f8d30ad02a703720c0545da3bc23af5a9
Walid Budgaga <wbudgaga@gmail.com>
2020-09-02 11:56:27 -0600

    Update WireformatesTest.java

--- mr/dht/peer2peernetwork/test/WireformatesTest.java
+++ mr/dht/peer2peernetwork/test/WireformatesTest.java
@@ -27,7 +27,7 @@ public class WireformatesTest {
 	}
 
 	public static void testPeerInfoHandler() throws InstantiationException, IllegalAccessException, ClassNotFoundException{
-		PeerInfo msg = new PeerInfo();
+		PeerInfo msg 			= new PeerInfo();
 		msg.setPeer(getPeerData(876519041972L,"Walid S.","red-rock",  1093 ));	
 		byte[] bytes = msg.packMessage();
 		System.out.println(msg.getMessageID()+"  "+msg.getHandlerID()+"  "+ msg.getPeer().getPeerID()+"  "+msg.getPeer().getNickName()+"  "+msg.getPeer().getHost()+"  "+msg.getPeer().getPortNum()+"  ");
