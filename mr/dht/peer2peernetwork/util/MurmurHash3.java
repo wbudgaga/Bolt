8aa156a4f3775f291b4823685c4517fb7432c39b
Walid Budgaga <wbudgaga@gmail.com>
2020-09-04 12:56:14 -0600

    Update MurmurHash3.java

--- mr/dht/peer2peernetwork/util/MurmurHash3.java
+++ mr/dht/peer2peernetwork/util/MurmurHash3.java
@@ -211,9 +211,9 @@ public final class MurmurHash3 {
     // fmix(h1);
     h1 					^= h1 >>> 16;
     h1 					*= 0x85ebca6b;
-    h1 ^= h1 >>> 13;
-    h1 *= 0xc2b2ae35;
-    h1 ^= h1 >>> 16;
+    h1 					^= h1 >>> 13;
+    h1 					*= 0xc2b2ae35;
+    h1 					^= h1 >>> 16;
 
     return h1;
   }
