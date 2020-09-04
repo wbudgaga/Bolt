0e31d159df4f34df6da28f6a4e1d7d40320923b6
Walid Budgaga <wbudgaga@gmail.com>
2020-09-04 12:41:01 -0600

    Update MurmurHash3.java

--- mr/dht/peer2peernetwork/util/MurmurHash3.java
+++ mr/dht/peer2peernetwork/util/MurmurHash3.java
@@ -63,8 +63,8 @@ public final class MurmurHash3 {
   /** Returns the MurmurHash3_x86_32 hash. */
   public static int murmurhash3_x86_32(byte[] data, int offset, int len, int seed) {
 
-    final int c1 = 0xcc9e2d51;
-    final int c2 = 0x1b873593;
+    final int c1 			= 0xcc9e2d51;
+    final int c2 			= 0x1b873593;
 
     int h1 = seed;
     int roundedEnd = offset + (len & 0xfffffffc);  // round down to 4 byte block
