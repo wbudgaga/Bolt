ac9f577f61989171f7f6d9aabfb9b62e23c6943c
Walid Budgaga <wbudgaga@gmail.com>
2020-09-04 13:00:46 -0600

    Update MurmurHash3.java

--- mr/dht/peer2peernetwork/util/MurmurHash3.java
+++ mr/dht/peer2peernetwork/util/MurmurHash3.java
@@ -223,80 +223,84 @@ public final class MurmurHash3 {
   public static void murmurhash3_x64_128(byte[] key, int offset, int len, int seed, LongPair out) {
     // The original algorithm does have a 32 bit unsigned seed.
     // We have to mask to match the behavior of the unsigned types and prevent sign extension.
-    long h1 = seed & 0x00000000FFFFFFFFL;
-    long h2 = seed & 0x00000000FFFFFFFFL;
+    long h1 				= seed & 0x00000000FFFFFFFFL;
+    long h2 				= seed & 0x00000000FFFFFFFFL;
 
-    final long c1 = 0x87c37b91114253d5L;
-    final long c2 = 0x4cf5ad432745937fL;
+    final long c1 			= 0x87c37b91114253d5L;
+    final long c2 			= 0x4cf5ad432745937fL;
 
-    int roundedEnd = offset + (len & 0xFFFFFFF0);  // round down to 16 byte block
-    for (int i=offset; i<roundedEnd; i+=16) {
-        long k1 = getLongLittleEndian(key, i);
-        long k2 = getLongLittleEndian(key, i+8);
-        k1 *= c1; k1  = Long.rotateLeft(k1,31); k1 *= c2; h1 ^= k1;
-        h1 = Long.rotateLeft(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
-        k2 *= c2; k2  = Long.rotateLeft(k2,33); k2 *= c1; h2 ^= k2;
-        h2 = Long.rotateLeft(h2,31); h2 += h1; h2 = h2*5+0x38495ab5;
+    int roundedEnd 			= offset + (len & 0xFFFFFFF0);  // round down to 16 byte block
+    for (int i				= offset; i<roundedEnd; i+=16) {
+        long k1 			= getLongLittleEndian(key, i);
+        long k2 			= getLongLittleEndian(key, i+8);
+        k1 				*= c1; 
+	k1  				= Long.rotateLeft(k1,31); k1 *= c2; h1 ^= k1;
+        h1 				= Long.rotateLeft(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
+        k2 				*= c2; k2  = Long.rotateLeft(k2,33); k2 *= c1; h2 ^= k2;
+        h2 				= Long.rotateLeft(h2,31); h2 += h1; h2 = h2*5+0x38495ab5;
     }
 
-    long k1 = 0;
-    long k2 = 0;
+    long k1 				= 0;
+    long k2 				= 0;
 
     switch (len & 15) {
-      case 15: k2  = (key[roundedEnd+14] & 0xffL) << 48;
-      case 14: k2 |= (key[roundedEnd+13] & 0xffL) << 40;
-      case 13: k2 |= (key[roundedEnd+12] & 0xffL) << 32;
-      case 12: k2 |= (key[roundedEnd+11] & 0xffL) << 24;
-      case 11: k2 |= (key[roundedEnd+10] & 0xffL) << 16;
-      case 10: k2 |= (key[roundedEnd+ 9] & 0xffL) << 8;
-      case  9: k2 |= (key[roundedEnd+ 8] & 0xffL);
-        k2 *= c2; k2  = Long.rotateLeft(k2, 33); k2 *= c1; h2 ^= k2;
-      case  8: k1  = ((long)key[roundedEnd+7]) << 56;
-      case  7: k1 |= (key[roundedEnd+6] & 0xffL) << 48;
-      case  6: k1 |= (key[roundedEnd+5] & 0xffL) << 40;
-      case  5: k1 |= (key[roundedEnd+4] & 0xffL) << 32;
-      case  4: k1 |= (key[roundedEnd+3] & 0xffL) << 24;
-      case  3: k1 |= (key[roundedEnd+2] & 0xffL) << 16;
-      case  2: k1 |= (key[roundedEnd+1] & 0xffL) << 8;
-      case  1: k1 |= (key[roundedEnd  ] & 0xffL);
-        k1 *= c1; k1  = Long.rotateLeft(k1,31); k1 *= c2; h1 ^= k1;
+      case 15: k2  			= (key[roundedEnd+14] & 0xffL) << 48;
+      case 14: k2 			|= (key[roundedEnd+13] & 0xffL) << 40;
+      case 13: k2 			|= (key[roundedEnd+12] & 0xffL) << 32;
+      case 12: k2 			|= (key[roundedEnd+11] & 0xffL) << 24;
+      case 11: k2 			|= (key[roundedEnd+10] & 0xffL) << 16;
+      case 10: k2 			|= (key[roundedEnd+ 9] & 0xffL) << 8;
+      case  9: k2 			|= (key[roundedEnd+ 8] & 0xffL);
+        	k2 			*= c2; 
+		k2  			= Long.rotateLeft(k2, 33); 
+		k2 			*= c1; 
+		h2 			^= k2;
+      case  8: k1  			= ((long)key[roundedEnd+7]) << 56;
+      case  7: k1 			|= (key[roundedEnd+6] & 0xffL) << 48;
+      case  6: k1 			|= (key[roundedEnd+5] & 0xffL) << 40;
+      case  5: k1 			|= (key[roundedEnd+4] & 0xffL) << 32;
+      case  4: k1 			|= (key[roundedEnd+3] & 0xffL) << 24;
+      case  3: k1 			|= (key[roundedEnd+2] & 0xffL) << 16;
+      case  2: k1 			|= (key[roundedEnd+1] & 0xffL) << 8;
+      case  1: k1 			|= (key[roundedEnd  ] & 0xffL);
+        k1 				*= c1; k1  = Long.rotateLeft(k1,31); k1 *= c2; h1 ^= k1;
     }
 
     //----------
     // finalization
 
-    h1 ^= len; h2 ^= len;
+    h1 					^= len; h2 ^= len;
 
-    h1 += h2;
-    h2 += h1;
+    h1 					+= h2;
+    h2 					+= h1;
 
-    h1 = fmix64(h1);
-    h2 = fmix64(h2);
+    h1 					= fmix64(h1);
+    h2 					= fmix64(h2);
 
-    h1 += h2;
-    h2 += h1;
+    h1 					+= h2;
+    h2 					+= h1;
 
-    out.val1 = h1;
-    out.val2 = h2;
+    out.val1			 	= h1;
+    out.val2 				= h2;
   }
   public static long hash32(final long data) {
-	  byte[] bytes = ByteStream.longToByteArray(data);  
+	  byte[] bytes 			= ByteStream.longToByteArray(data);  
 	  return murmurhash3_x86_32 ( bytes,0, bytes.length, seed);
   }
   public static long hash32(final String data) { 
-	  byte[] bytes = data.getBytes();
+	  byte[] bytes 			= data.getBytes();
 	  return murmurhash3_x86_32 ( bytes,0, bytes.length, seed);
   }
   
 	public static long hash128Low(final long data) {
-		byte[] bytes = ByteStream.longToByteArray(data);
+		byte[] bytes 		= ByteStream.longToByteArray(data);
 		synchronized(LOCK){
 			murmurhash3_x64_128( bytes,0, bytes.length, seed,out);
 			return out.val1;
 		}
 	}
 	public static long hash128Low(final String data) {
-		byte[] bytes = data.getBytes();
+		byte[] bytes 		= data.getBytes();
 		synchronized(LOCK){
 			murmurhash3_x64_128( bytes,0, bytes.length, seed,out);
 			return out.val1;
