f02d08e187aef6214231b03c8b8be3db56c48baa
Walid Budgaga <wbudgaga@gmail.com>
2020-09-04 12:39:45 -0600

    Update MurmurHash3.java

--- mr/dht/peer2peernetwork/util/MurmurHash3.java
+++ mr/dht/peer2peernetwork/util/MurmurHash3.java
@@ -25,17 +25,16 @@ public final class MurmurHash3 {
     public long val2;
   }
   
-  public static final LongPair out = new LongPair();
-  
-  public static final Object LOCK = new Object();
-  public static final int seed = 0xe17a1465;
+  public static final LongPair out 	= new LongPair();
+  public static final Object LOCK 	= new Object();
+  public static final int seed 		= 0xe17a1465;
 
   public static final int fmix32(int h) {
-    h ^= h >>> 16;
-    h *= 0x85ebca6b;
-    h ^= h >>> 13;
-    h *= 0xc2b2ae35;
-    h ^= h >>> 16;
+    h 					^= h >>> 16;
+    h 					*= 0x85ebca6b;
+    h 					^= h >>> 13;
+    h 					*= 0xc2b2ae35;
+    h 					^= h >>> 16;
     return h;
   }
 
