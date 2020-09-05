b56d0646cd28f349008a244c41e040702e667e4e
Walid Budgaga <wbudgaga@gmail.com>
2020-09-05 14:32:43 -0600

    Update MurmurHashing.java

--- mr/dht/peer2peernetwork/util/MurmurHashing.java
+++ mr/dht/peer2peernetwork/util/MurmurHashing.java
@@ -17,19 +17,19 @@ public final class MurmurHashing {
 		final int m 			= 0x5bd1e995;
 		final int r 			= 24;
 		// Initialize the hash to a random value
-		int h = seed^length;
-		int length4 = length/4;
+		int h 				= seed^length;
+		int length4 			= length/4;
 		for (int i = 0; i < length4; i++) {
 			final int i4 		= i*4;
 			int k 			= (data[i4+0]&0xff) + ((data[i4+1]&0xff)<<8) + ((data[i4+2]&0xff)<<16) + ((data[i4+3]&0xff)<<24);
 			k 			*= m;
 			k 			^= k >>> r;
 			k 			*= m;
-			h *= m;
-			h ^= k;
+			h 			*= m;
+			h 			^= k;
 		}
 			
-			// Handle the last few bytes of the input array
+		// Handle the last few bytes of the input array
 			switch (length%4) {
 			case 3: h ^= (data[(length&~3) +2]&0xff) << 16;
 			case 2: h ^= (data[(length&~3) +1]&0xff) << 8;
