93612ece7d42ee056b16ed39f47df4c2dd429922
Walid Budgaga <wbudgaga@gmail.com>
2020-09-05 14:29:18 -0600

    Update MurmurHashing.java

--- mr/dht/peer2peernetwork/util/MurmurHashing.java
+++ mr/dht/peer2peernetwork/util/MurmurHashing.java
@@ -12,13 +12,13 @@ public final class MurmurHashing {
 	* @return 32 bit hash of the given array
 	*/
 	public static int hash32( final byte[] data, int length, int seed) {
-			// 'm' and 'r' are mixing constants generated offline.
-			// They're not really 'magic', they just happen to work well.
-			final int m = 0x5bd1e995;
-			final int r = 24;
-			// Initialize the hash to a random value
-			int h = seed^length;
-			int length4 = length/4;
+		// 'm' and 'r' are mixing constants generated offline.
+		// They're not really 'magic', they just happen to work well.
+		final int m = 0x5bd1e995;
+		final int r = 24;
+		// Initialize the hash to a random value
+		int h = seed^length;
+		int length4 = length/4;
 
 			for (int i=0; i<length4; i++) {
 				final int i4 = i*4;
