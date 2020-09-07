91c7179aa7ca1bf87d9a495f5a6240421f780886
Walid Budgaga <wbudgaga@gmail.com>
2020-09-07 14:39:31 -0600

    Update GetPredecessor.java

--- mr/dht/peer2peernetwork/wireformates/GetPredecessor.java
+++ mr/dht/peer2peernetwork/wireformates/GetPredecessor.java
@@ -2,7 +2,6 @@ package mr.dht.peer2peernetwork.wireformates;
 
 import mr.dht.peer2peernetwork.nodes.PeerData;
 
-
 public class GetPredecessor extends PeerInfo{
 	public GetPredecessor() {
 		super(GET_PREDECESSOR, GET_PREDECESSOR);
