a09c1af78bd62d1612a8b8cb216f76dd4154e772
Walid Budgaga <wbudgaga@gmail.com>
2020-09-07 14:41:35 -0600

    Update GetPredecessorResponse.java

--- mr/dht/peer2peernetwork/wireformates/GetPredecessorResponse.java
+++ mr/dht/peer2peernetwork/wireformates/GetPredecessorResponse.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import mr.dht.peer2peernetwork.handlers.MessageHandler;
 
 public class GetPredecessorResponse extends PeerInfo{
+	
 	public GetPredecessorResponse() {
 		super(GET_PREDECESSOR_RESPONSE, GET_PREDECESSOR_RESPONSE);
 	}
