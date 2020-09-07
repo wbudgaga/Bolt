94283a46495d477a3a981ffe06f9e6fc3ae6821d
Walid Budgaga <wbudgaga@gmail.com>
2020-09-07 14:36:42 -0600

    Update GetAllPeers.java

--- mr/dht/peer2peernetwork/wireformates/GetAllPeers.java
+++ mr/dht/peer2peernetwork/wireformates/GetAllPeers.java
@@ -3,7 +3,6 @@ package mr.dht.peer2peernetwork.wireformates;
 //this class represents a RegisterRequest message. It is used to pack and unpack the RegisterRequest message by using the methods in superclass
 //the method handle() calls the method handle() in receiver to handle the message
 public class GetAllPeers extends RegisterRequest{
-	
 	public GetAllPeers() {
 		super(GET_ALLPEERS, GET_ALLPEERS);
 	}
