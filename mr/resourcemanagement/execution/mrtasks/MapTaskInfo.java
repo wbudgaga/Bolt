68449bbed6b79fe454ea0d69d46a95dd72fb9aeb
Walid Budgaga <wbudgaga@gmail.com>
2020-09-12 12:45:00 -0600

    Update MapTaskInfo.java

--- mr/resourcemanagement/execution/mrtasks/MapTaskInfo.java
+++ mr/resourcemanagement/execution/mrtasks/MapTaskInfo.java
@@ -22,15 +22,17 @@ public class MapTaskInfo<K1,V1,K2,V2> extends TaskInfo<K1,V1,K2,V2>{
 	}
 		
 	public DataSource getDataReader(ThreadPoolManager ioThreadPool) throws FileNotFoundException, InterruptedException{
-		DataSource fr 			= new TextFileReader(chunkName);
+		DataSource fr 					= new TextFileReader(chunkName);
 		fr.setIOThreadPool(ioThreadPool);
 		fr.setBuffer(mrTask.pendingQueue);
 		return  fr;
 	}
+	
 	public Partitioner<K2,V2>  getPartitioner() throws InstantiationException, IllegalAccessException, MalformedURLException, ClassNotFoundException{
-		Class<Partitioner<K2,V2>> partitionerClass = ClassLoader.loadPartitioner(null, partitionerClassName);
+		Class<Partitioner<K2,V2>> partitionerClass 	= ClassLoader.loadPartitioner(null, partitionerClassName);
 		return partitionerClass.newInstance();
-	}	
+	}
+	
 	public String getPartitionerClassName() {
 		return partitionerClassName;
 	}
