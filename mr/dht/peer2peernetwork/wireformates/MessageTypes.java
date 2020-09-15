bc0bedc69107e0640be38ab972a8c7e95a856365
Walid Budgaga <wbudgaga@gmail.com>
2020-09-15 14:19:36 -0600

    Update MessageTypes.java

--- mr/dht/peer2peernetwork/wireformates/MessageTypes.java
+++ mr/dht/peer2peernetwork/wireformates/MessageTypes.java
@@ -17,8 +17,8 @@ public interface MessageTypes {
 	public static final int QUERY_RESULT				= 7;
 	public static final int GET_PREDECESSOR		 		= 8;
 	public static final int RANDOM_PEER	 			= 9;
-	public static final int STORE_FILE_REQUEST	 			= 10;
-	public static final int GET					 			= 11;
+	public static final int STORE_FILE_REQUEST	 		= 10;
+	public static final int GET					= 11;
 	public static final int GET_RESPONSE		 			= 12;
 	public static final int GET_SUCCESSOR		 			= 13;
 	public static final int GET_PREDECESSOR_RESPONSE	 	= 14;
