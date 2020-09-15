4f85aaaf07821f9960e27595dded1f4093068244
Walid Budgaga <wbudgaga@gmail.com>
2020-09-15 14:17:53 -0600

    Update MessageTypes.java

--- mr/dht/peer2peernetwork/wireformates/MessageTypes.java
+++ mr/dht/peer2peernetwork/wireformates/MessageTypes.java
@@ -4,8 +4,8 @@ package mr.dht.peer2peernetwork.wireformates;
 // contains the messages types and their equivalent classes' names
 // it is very important to keep the order the same in both lists 
 public interface MessageTypes {
-	public static final byte SUCCEESS 						= 1;
-	public static final byte FAILURE 						= 2;
+	public static final byte SUCCEESS 				= 1;
+	public static final byte FAILURE 				= 2;
 	
 	public static final int REGISTER_REQUEST 				= 0;
 	public static final int REGISTER_RESPONSE 				= 1;
