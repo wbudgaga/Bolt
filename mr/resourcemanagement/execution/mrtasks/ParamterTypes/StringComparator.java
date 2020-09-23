0d7ef8559699fb7d69ae1d075714db39be48e34e
Walid Budgaga <wbudgaga@gmail.com>
2020-09-23 11:05:07 -0600

    Update StringComparator.java

--- mr/resourcemanagement/execution/mrtasks/ParamterTypes/StringComparator.java
+++ mr/resourcemanagement/execution/mrtasks/ParamterTypes/StringComparator.java
@@ -3,6 +3,7 @@ package mr.resourcemanagement.execution.mrtasks.ParamterTypes;
 import java.util.Comparator;
 
 public class StringComparator implements Comparator<String> {
+	
 	public int compare(String text1, String text2) {
 		return text1.compareTo(text2);
 	}
