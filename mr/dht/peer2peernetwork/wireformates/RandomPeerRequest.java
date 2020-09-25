7aa1990417028a5ddeebaf7b159928718340c176
Walid Budgaga <wbudgaga@gmail.com>
2020-09-25 11:13:49 -0600

    Update RandomPeerRequest.java

--- mr/dht/peer2peernetwork/wireformates/RandomPeerRequest.java
+++ mr/dht/peer2peernetwork/wireformates/RandomPeerRequest.java
@@ -3,7 +3,6 @@ package mr.dht.peer2peernetwork.wireformates;
 //this class represents a RegisterRequest message. It is used to pack and unpack the RegisterRequest message by using the methods in superclass
 //the method handle() calls the method handle() in receiver to handle the message
 public class RandomPeerRequest extends PeerInfo{
-	
 	public RandomPeerRequest() {
 		super(RANDOM_PEER_REQUEST, RANDOM_PEER_REQUEST);
 	}
