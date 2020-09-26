5d9abb06263b0043881b104293386583ab3b7dc8
Walid Budgaga <wbudgaga@gmail.com>
2020-09-26 11:26:17 -0600

    Update StartJobBatch.java

--- mr/dht/peer2peernetwork/wireformates/StartJobBatch.java
+++ mr/dht/peer2peernetwork/wireformates/StartJobBatch.java
@@ -2,8 +2,7 @@ package mr.dht.peer2peernetwork.wireformates;
 
 //this class represents a RegisterRequest message. It is used to pack and unpack the RegisterRequest message by using the methods in superclass
 //the method handle() calls the method handle() in receiver to handle the message
-public class StartJobBatch extends Request{
-	
+public class StartJobBatch extends Request{	
 	public StartJobBatch() {
 		super(START_JOB_BATCH, START_JOB_BATCH);
 	}
